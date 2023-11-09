8336455be9109fc777fc4299b89f705c7578a60d
Héctor Sandoval <ingsan@live.com.mx>
2023-11-09 13:40:49 -0600

    Day 60

---
+++ FriendFace/DetailUserView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailUserView.swift
+//  FriendFace
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 09/11/23.
+//
+
+import SwiftUI
+
+struct DetailUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailUserView()
+}
